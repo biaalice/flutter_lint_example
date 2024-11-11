2b787f09ec5e0de4b4b0336efbe5e7ea70b6b28f
biaalice <bia.alice@softdesign.com.br>
2024-11-11 11:49:42 -0300

    FEAT: add smelly code

---
+++ lib/example/example.dart
@@ -0,0 +1,13 @@
+void main() {
+  // List<int> numbers = List.generate(1000000, (index) => index);
+
+  // int result = -1;
+  // for (var i = 0; i < numbers.length; i++) {
+  //   if (numbers[i] == 500000) {
+  //     result = numbers[i];
+  //     break;
+  //   }
+  // }
+
+  // print(result);
+}
